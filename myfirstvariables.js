564aa113063ccc5fbe2ca0537bb46fb7c2d9951b
Marios Athanasiou <90323650+marioathan99@users.noreply.github.com>
2022-12-21 19:57:57 +0200

    Update myfirstvariables.js

--- myfirstvariables.js
+++ myfirstvariables.js
@@ -1,3 +1,6 @@
 // Define a variable called myLuckyNumber and set it equal to 37
 let myLuckyNumber = 37;
 let octopusLimbs = 8; 
+// Define a constant called boilingPointC and set it to 100 and define a second constant called boilingPointF and set it to 212.  
+const boilingPointC = 100;
+const boilingPointF = 212; 
