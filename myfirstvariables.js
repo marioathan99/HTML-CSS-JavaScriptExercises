99f490d80bb71bf78e0c2c00f35d85bfbc4a7846
Marios Athanasiou <90323650+marioathan99@users.noreply.github.com>
2023-01-01 21:53:27 +0200

    Update myfirstvariables.js

--- myfirstvariables.js
+++ myfirstvariables.js
@@ -4,3 +4,6 @@ let octopusLimbs = 8;
 // Define a constant called boilingPointC and set it to 100 and define a second constant called boilingPointF and set it to 212.  
 const boilingPointC = 100;
 const boilingPointF = 212; 
+//Define your first strings
+let bestColor = "purple";
+let quote = 'You had me at "hello"'
