cbfa831cb5a5620a2c94645e59b6ae61e24c84f5
Marios Athanasiou <90323650+marioathan99@users.noreply.github.com>
2023-01-31 15:05:41 +0200

    Create TemplateLiterals.js

---
+++ TemplateLiterals.js
@@ -0,0 +1,3 @@
+const die1 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+const die2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+const roll = `You rolled a ${die1} and a ${die2}. They sum to ${die1 + die2}`;
