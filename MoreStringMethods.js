440f8c52750b9f9705ec2e448f7243f9bc7629b2
Marios Athanasiou <90323650+marioathan99@users.noreply.github.com>
2023-01-31 14:33:01 +0200

    Create MoreStringMethods.js

---
+++ MoreStringMethods.js
@@ -0,0 +1,6 @@
+//I've provided you with a word variable, set to "skateboard".  Your goal is to use string methods on word, so that you end up with the string "beard". 
+//1.Use a string method to extract the "board" part of "skateboard"  (using the slice method)
+//2.Replace the "o" in board with an "e" (using the replace method)
+word.slice(5,10);
+word.replace("o", "e");
+let facialHair = "beard";
