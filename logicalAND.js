a4c61bd5640a35a74ed5ac8180db4ed255ac5a38
Marios Athanasiou <90323650+marioathan99@users.noreply.github.com>
2023-02-13 17:00:53 +0200

    Create logicalAND.js

---
+++ logicalAND.js
@@ -0,0 +1,6 @@
+//Create a mystery variable and a conditional for it which will print "YOU GOT IT!!!" only if the first letter is 'P', the length is 5+ characters and has the number 7
+
+const mystery = ''; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
+if(mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1){
+    console.log("YOU GOT IT!!!");
+}
